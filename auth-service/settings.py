238bdc47211de0f34d1c55d8767bdcbc82d87b3f
suman <sumangaire52@gmail.com>
2025-08-18 21:07:00 +0545

    auth-service/setup initial folder structure

---
+++ auth-service/settings.py
@@ -0,0 +1,13 @@
+import os
+
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = "your-database-url"
+    SECRET_KEY: str = "your-secret-key"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    ALGORITHM: str = "HS256"
+
+    class Config:
+        env_file = ".env"
